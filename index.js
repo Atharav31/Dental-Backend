d0a4150dcdfc5882f2de9ff1fe92bf0998d182f5
shashank shrivastava <internstudent02@gmail.com>
2025-03-02 18:19:46 +0530

    fixed-port

--- index.js
+++ index.js
@@ -25,4 +25,4 @@ app.use("/api/v1", appointmentRouter);
 app.use("/api/v1", prescriptionRouter);
 app.use("/api/v1", dashboardRouter);
 
-app.listen(8000, () => console.log("Server started on port 8000"));
+app.listen(3000, () => console.log("Server started on port 3000"));
