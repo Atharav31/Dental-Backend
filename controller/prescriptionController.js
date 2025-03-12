b26fde2a70255133b3f84671edfda7d967dcb445
shashank shrivastava <internstudent02@gmail.com>
2025-03-13 01:23:53 +0530

    isLoggedIn

--- controller/prescriptionController.js
+++ controller/prescriptionController.js
@@ -76,3 +76,23 @@ exports.updatePrescription = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+exports.loginAdmin = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const userAuthenticate = password === "admin1234" && userName === "admin";
+    if (userAuthenticate) {
+      res.status(200).json({
+        message: "Login Successfull",
+        isLoggedIn: true,
+      });
+    }
+    res.status(401).json({
+      message: "Login Failed",
+      isLoggedIn: false,
+    });
+  } catch (error) {
+    console.error("Error updating prescription:", error.message);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
--- routes/prescription.js
+++ routes/prescription.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { prescription, updatePrescription, getPrescriptionByAppointmentId } = require("../controller/prescriptionController");
+const {
+  prescription,
+  updatePrescription,
+  getPrescriptionByAppointmentId,
+  loginAdmin,
+} = require("../controller/prescriptionController");
 const prescriptionRouter = express.Router();
 
 prescriptionRouter.post("/prescription", prescription);
 prescriptionRouter.get("/prescription", getPrescriptionByAppointmentId);
 prescriptionRouter.patch("/prescription/:prescriptionId", updatePrescription);
+prescriptionRouter.post("/login", loginAdmin);
 
 module.exports = prescriptionRouter;
