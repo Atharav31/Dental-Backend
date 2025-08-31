0368ac9e0b6c64c4d1babb28575bcf54e1de5f7c
Atharav31 <atharavuttekar@gmail.com>
2025-09-01 00:18:17 +0530

    added treatment in precription model

--- models/Prescription.js
+++ models/Prescription.js
@@ -1,48 +1,67 @@
 const mongoose = require("mongoose");
 
-const prescriptionSchema = new mongoose.Schema(
-  {
-    appointmentId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Appointment",
-      required: true,
-    },
-    medicines: [
-      {
-        name: { type: String, required: true },
-        dosage: { type: String, required: true },
-        frequency: { type: String, required: true },
-        duration: { type: String, required: true },
-      },
-    ],
-    instructions: {
-      type: [String],
-    },
-    proceduresPerformed: [
-      {
-        procedureName: { type: String, required: true },
-        notes: { type: String },
-      },
-    ],
-    allergies: {
-      type: [String],
-      default: [],
-    },
-    followUpRequired: {
-      type: Boolean,
-      default: false,
-    },
-    nextVisit: {
-      type: Date,
-      required: true,
-    },
-    additionalNotes: {
-      type: String,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+const prescriptionSchema = new mongoose.Schema({
+	appointmentId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "Appointment",
+		required: true,
+	},
+
+	treatments: [
+		{
+			treatment: {
+				type: String,
+				enum: [
+					"general_checkup",
+					"root_canal",
+					"crown",
+					"cosmetic_procedure",
+					"filling",
+					"dental_implant",
+					"teeth_replacement",
+					"extraction",
+					"mouth_guard",
+					"child_procedure",
+					"gum_care",
+					"orthodontic",
+					"x_ray",
+				],
+			},
+			toothNumber: { type: String },
+		},
+	],
+	medicines: [
+		{
+			type: { type: String },
+			name: { type: String },
+			dosage: { type: String },
+			frequency: { type: String },
+			duration: { type: String },
+		},
+	],
+	instructions: {
+		type: [String],
+	},
+	proceduresPerformed: [
+		{
+			procedureName: { type: String },
+			notes: { type: String },
+		},
+	],
+	allergies: {
+		type: [String],
+		default: [],
+	},
+	followUpRequired: {
+		type: Boolean,
+		default: false,
+	},
+	nextVisit: {
+		type: Date,
+	},
+	additionalNotes: {
+		type: String,
+	},
+});
 
 module.exports = mongoose.model("Prescription", prescriptionSchema);
